e72d73aa302fbd568ab270c719144c7edd9a0108
Nekarone <yifei_yan@asl.org>
2024-01-14 22:23:27 +0000

    Removed Kon's motors (I'm crying)

--- src/main/java/frc/robot/layout/OperatorMap.java
+++ src/main/java/frc/robot/layout/OperatorMap.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.ExampleConfig;
 import frc.robot.core.util.controllers.CommandMap;
 import frc.robot.core.util.controllers.GameController;
-import frc.robot.subsystems.KonsMotors;
 
 public abstract class OperatorMap extends CommandMap {
 
@@ -17,10 +16,6 @@ public abstract class OperatorMap extends CommandMap {
   abstract JoystickButton getIntakeButton();
 
   private void registerIntake() {
-    if (ExampleConfig.Subsystems.INTAKE_ENABLED) {
-      KonsMotors newMotors = KonsMotors.getInstance();
-      getIntakeButton().onTrue(newMotors.run());
-    }
   }
 
   @Override
