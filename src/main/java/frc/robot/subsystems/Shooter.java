fd65bdfa7865ac7d7e1a59c94a6374b4c81a388c
danielkhoury23 <daniel_khoury@asl.org>
2024-01-20 15:52:02 +0000

    comments

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -9,37 +9,38 @@ import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.RobotMap.ShooterMap;
 
 public class Shooter {
-    private CANSparkMax launchMotor;
-    private CANSparkMax feedMotor;
+    private CANSparkMax launchMotor; //declaration of launch motor
+    private CANSparkMax feedMotor;// declaration of feed motor
     private static Shooter instance;
 
     private Shooter() {
-        launchMotor = new CANSparkMax(ShooterMap.LAUNCH_MOTOR_ID, MotorType.kBrushless);
-        feedMotor = new CANSparkMax(ShooterMap.FEED_MOTOR_ID, MotorType.kBrushless);
+        launchMotor = new CANSparkMax(ShooterMap.LAUNCH_MOTOR_ID, MotorType.kBrushless); // assigns launchmotor variable to CANSparkMax motor
+        feedMotor = new CANSparkMax(ShooterMap.FEED_MOTOR_ID, MotorType.kBrushless);// assigns FeedMotor variable to anotherCANSparkMax motor
     }
 
+    // making sure only one instance of shooter exists
     public static Shooter getInstance() {
         if (instance == null)
             instance = new Shooter();
         return instance;
 
     }
-
+// setting launch motor speed
     public void setLaunchSpeed(double speed) {
         launchMotor.set(speed);
 
     }
-
+// setting feed motor speed
     public void setFeedSpeed(double speed) {
         feedMotor.set(speed);
     }
-
+// method for stopping both motors at the same time 
     public void stopMotors() {
         launchMotor.set(0);
         feedMotor.set(0);
 
     }
-
+// sets motor speed to negative values so shooting mechanism can activate
     public Command runShooter() {
         return new StartEndCommand(
                 () -> {
@@ -49,12 +50,12 @@ public class Shooter {
                 () -> stopMotors());
 
     }
-
+//preparing launch motor for shooting
     public Command prepareShootCommand() {
         return new InstantCommand(() -> setLaunchSpeed(ShooterMap.launchSpeed));
 
     }
-
+// command for launching the note by activating the motors 
     public Command launchNoteCommand() {
         return new InstantCommand(
                 () -> {
