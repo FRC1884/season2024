c7aadede0011737a2a2752cabf50b0319ec4c2c6
Nekarone <yifei_yan@asl.org>
2024-03-17 12:52:28 +0000

    Fixed an error with formatting AutoCommands

--- src/main/java/frc/robot/auto/AutoCommands.java
+++ src/main/java/frc/robot/auto/AutoCommands.java
@@ -2,7 +2,6 @@ package frc.robot.auto;
 
 import com.pathplanner.lib.auto.NamedCommands;
 
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -24,37 +23,43 @@ import java.sql.Driver;
 import java.util.function.Supplier;
 
 public class AutoCommands {
-    public static void registerAutoCommands() {
-        FlywheelLookupTable lookupTable = FlywheelLookupTable.getInstance();
+        public static void registerAutoCommands() {
+                FlywheelLookupTable lookupTable = FlywheelLookupTable.getInstance();
 
-        // default to red speaker to avoid null pointer exception
-        Supplier<Pose2d> getTarget = () -> DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == DriverStation.Alliance.Blue
-                ? Coordinates.BLUE_SPEAKER : Coordinates.RED_SPEAKER;
+                // default to red speaker to avoid null pointer exception
+                Supplier<Pose2d> getTarget = () -> DriverStation.getAlliance().isPresent()
+                                && DriverStation.getAlliance().get() == DriverStation.Alliance.Blue
+                                                ? Coordinates.BLUE_SPEAKER
+                                                : Coordinates.RED_SPEAKER;
 
-        PoseEstimator poseEstimator = PoseEstimator.getInstance();
-        Pivot pivot = Pivot.getInstance();
-        Shooter shooter = Shooter.getInstance();
-        Feeder feeder = Feeder.getInstance();
+                PoseEstimator poseEstimator = PoseEstimator.getInstance();
+                Pivot pivot = Pivot.getInstance();
+                Shooter shooter = Shooter.getInstance();
+                Feeder feeder = Feeder.getInstance();
 
-        NamedCommands.registerCommand("Intake", new IntakeUntilLoadedCommand());
+                NamedCommands.registerCommand("Intake", new IntakeUntilLoadedCommand());
 
-        NamedCommands.registerCommand("SpoolShooter", shooter.setFlywheelVelocityCommand(
-                () -> lookupTable.get(poseEstimator.getDistanceToPose(getTarget.get().getTranslation())).getRPM()
-        ));
+                NamedCommands.registerCommand("SpoolShooter", shooter.setFlywheelVelocityCommand(
+                                () -> lookupTable.get(poseEstimator.getDistanceToPose(getTarget.get().getTranslation()))
+                                                .getRPM()));
 
-        NamedCommands.registerCommand("Pivot", pivot.updatePosition(
-                () -> lookupTable.get(poseEstimator.getDistanceToPose(getTarget.get().getTranslation())).getAngleSetpoint()
-        ));
+                NamedCommands.registerCommand("Pivot", pivot.updatePosition(
+                                () -> lookupTable.get(poseEstimator.getDistanceToPose(getTarget.get().getTranslation()))
+                                                .getAngle()));
 
-        NamedCommands.registerCommand("Shoot", new SequentialCommandGroup(
-                new InstantCommand(() -> feeder.setFeederState(FeederDirection.FORWARD), Feeder.getInstance()),
-                new WaitCommand(0.3),
-                new InstantCommand(() -> feeder.setFeederState(FeederDirection.STOPPED), Feeder.getInstance())));
+                NamedCommands.registerCommand("Shoot", new SequentialCommandGroup(
+                                new InstantCommand(() -> feeder.setFeederState(FeederDirection.FORWARD),
+                                                Feeder.getInstance()),
+                                new WaitCommand(0.3),
+                                new InstantCommand(() -> feeder.setFeederState(FeederDirection.STOPPED),
+                                                Feeder.getInstance())));
 
-        NamedCommands.registerCommand("VisionIntake", new IntakeUntilLoadedCommand().raceWith(Vision.getInstance().PIDtoNoteRobotRelativeCommand()));
-        // NamedCommands.registerCommand("Intake", new PrintCommand("Intake"));
-        // NamedCommands.registerCommand("SpoolShooter", new PrintCommand("Spooling"));
-        // NamedCommands.registerCommand("Shoot", new PrintCommand("Shoot"));
-        // NamedCommands.registerCommand("StopShooter", new PrintCommand("Stop Spooling"));
-    }
+                NamedCommands.registerCommand("VisionIntake", new IntakeUntilLoadedCommand()
+                                .raceWith(Vision.getInstance().PIDtoNoteRobotRelativeCommand()));
+                // NamedCommands.registerCommand("Intake", new PrintCommand("Intake"));
+                // NamedCommands.registerCommand("SpoolShooter", new PrintCommand("Spooling"));
+                // NamedCommands.registerCommand("Shoot", new PrintCommand("Shoot"));
+                // NamedCommands.registerCommand("StopShooter", new PrintCommand("Stop
+                // Spooling"));
+        }
 }
